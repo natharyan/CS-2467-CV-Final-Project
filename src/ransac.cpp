537e0eb93e8d22452c26d8ad1f749a1933afdec1
Manya Garg <mgmanyagarg1710@gmail.com>
2024-12-12 23:31:46 +0530

    add fundamental matrix

--- src/ransac.cpp
+++ src/ransac.cpp
@@ -1,74 +1,110 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
+#include <random>
+#include <Eigen/Dense>
 
-struct Point {
-    double x, y;
-};
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using Eigen::JacobiSVD;
+using Eigen::ComputeFullU;
+using Eigen::ComputeFullV;
 
-struct Line {
-    double slope, intercept;
-};
+// Helper function: Compute the fundamental matrix using the 8-point algorithm
+MatrixXd computeFundamentalMatrix(const std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>>& matches) {
+    MatrixXd A(matches.size(), 9);
+    for (size_t i = 0; i < matches.size(); ++i) {
+        double x1 = matches[i].first(0);
+        double y1 = matches[i].first(1);
+        double x2 = matches[i].second(0);
+        double y2 = matches[i].second(1);
 
-double calculateDistance(const Point& p, const Line& line) {
-    // Calculate perpendicular distance from a point to a line.
-    return std::abs(line.slope * p.x - p.y + line.intercept) / std::sqrt(line.slope * line.slope + 1);
+        A(i, 0) = x1 * x2;
+        A(i, 1) = x1 * y2;
+        A(i, 2) = x1;
+        A(i, 3) = y1 * x2;
+        A(i, 4) = y1 * y2;
+        A(i, 5) = y1;
+        A(i, 6) = x2;
+        A(i, 7) = y2;
+        A(i, 8) = 1.0;
+    }
+
+    JacobiSVD<MatrixXd> svd(A, ComputeFullU | ComputeFullV);
+    VectorXd f = svd.matrixV().col(8);
+
+    MatrixXd F(3, 3);
+    F << f(0), f(1), f(2),
+         f(3), f(4), f(5),
+         f(6), f(7), f(8);
+
+    JacobiSVD<MatrixXd> svdF(F, ComputeFullU | ComputeFullV);
+    VectorXd singularValues = svdF.singularValues();
+    singularValues(2) = 0; // Enforce rank 2
+    F = svdF.matrixU() * singularValues.asDiagonal() * svdF.matrixV().transpose();
+
+    return F;
 }
 
-Line fitLine(const Point& p1, const Point& p2) {
-    // Fit a line through two points
-    double slope = (p2.y - p1.y) / (p2.x - p1.x);
-    double intercept = p1.y - slope * p1.x;
-    return {slope, intercept};
-}
+// RANSAC to estimate the fundamental matrix
+MatrixXd ransacFundamentalMatrix(const std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>>& matches, int maxIterations, double threshold) {
+    int bestInlierCount = 0;
+    MatrixXd bestF;
+    std::random_device rd;
+    std::mt19937 gen(rd());
 
-Line ransac(const std::vector<Point>& points, int maxIterations, double distanceThreshold, int& bestInlierCount) {
-    Line bestLine = {0, 0};
-    bestInlierCount = 0;
-    std::srand(static_cast<unsigned>(std::time(0)));
+    for (int iter = 0; iter < maxIterations; ++iter) {
+        std::uniform_int_distribution<> dis(0, matches.size() - 1);
 
-    for (int i = 0; i < maxIterations; ++i) {
-        // Randomly select two points
-        int idx1 = std::rand() % points.size();
-        int idx2 = std::rand() % points.size();
-        while (idx1 == idx2) {
-            idx2 = std::rand() % points.size();
+        // Randomly sample 8 matches
+        std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>> sample;
+        for (int i = 0; i < 8; ++i) {
+            sample.push_back(matches[dis(gen)]);
         }
 
-        Line line = fitLine(points[idx1], points[idx2]);
-        int inlierCount = 0;
+        MatrixXd F = computeFundamentalMatrix(sample);
 
-        for (const auto& p : points) {
-            if (calculateDistance(p, line) < distanceThreshold) {
+        // Count inliers
+        int inlierCount = 0;
+        for (const auto& match : matches) {
+            Eigen::Vector3d p1(match.first(0), match.first(1), 1.0);
+            Eigen::Vector3d p2(match.second(0), match.second(1), 1.0);
+            
+            double error = std::abs(p2.transpose() * F * p1);
+            if (error < threshold) {
                 ++inlierCount;
             }
         }
 
         if (inlierCount > bestInlierCount) {
-            bestLine = line;
             bestInlierCount = inlierCount;
+            bestF = F;
         }
     }
 
-    return bestLine;
+    return bestF;
 }
 
 int main() {
-    // Example points (you can modify or extend this)
-    std::vector<Point> points = {
-        {1, 2}, {2, 4.1}, {3, 5.9}, {4, 8}, {5, 10}, {100, 100}, {101, 101}
+    // Example matches: pairs of corresponding points in two images
+    std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>> matches = {
+        {{100, 150}, {110, 160}},
+        {{200, 250}, {210, 260}},
+        {{300, 350}, {310, 360}},
+        {{400, 450}, {410, 460}},
+        {{500, 550}, {510, 560}},
+        {{600, 650}, {610, 660}},
+        {{700, 750}, {710, 760}},
+        {{800, 850}, {810, 860}},
+        {{900, 950}, {910, 960}},
+        {{1000, 1050}, {1010, 1060}}
     };
 
-    int maxIterations = 100;
-    double distanceThreshold = 1.0;
-    int bestInlierCount = 0;
+    int maxIterations = 1000;
+    double threshold = 0.01;
 
-    Line bestLine = ransac(points, maxIterations, distanceThreshold, bestInlierCount);
+    MatrixXd F = ransacFundamentalMatrix(matches, maxIterations, threshold);
 
-    std::cout << "Best line: y = " << bestLine.slope << "x + " << bestLine.intercept << "\n";
-    std::cout << "Number of inliers: " << bestInlierCount << "\n";
+    std::cout << "Estimated Fundamental Matrix:\n" << F << std::endl;
 
     return 0;
 }
